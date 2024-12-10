a7420e8f11f9cca06d6a48b947a77405b4767b61
mohamedjr21 <mohamedjr21@example.com>
2024-12-10 13:45:44 +0100

    Mejorando  el proyecto con una REAPI de tiempo. Funcionandoo

--- src/main/java/com/example/conexion_retrofit_gson/HelloController.java
+++ src/main/java/com/example/conexion_retrofit_gson/HelloController.java
@@ -1,5 +1,3 @@
-
-
 package com.example.conexion_retrofit_gson;
 
 import javafx.fxml.FXML;
@@ -13,37 +11,38 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class HelloController {
 
     @FXML
+    private TextField ciudadTextField;
+    @FXML
     private TextField mostrarDatosTextfield;
 
-    public void initialize() {
+    private final String apiKey = "9693c5a9f86ef1b1aa621cd546973a28";
+
+    @FXML
+    public void obtenerTiempo() {
+        String ciudad = ciudadTextField.getText();
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://api.openweathermap.org/data/2.5/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
         APIservicio apiService = retrofit.create(APIservicio.class);
-        Call<Tiempo> call = apiService.obtenerTiempo(
-                "36.7763",
-                "-2.8147",
-                "9693c5a9f86ef1b1aa621cd546973a28",
-                "metric"
-        );
+        Call<Tiempo> call = apiService.obtenerTiempo(ciudad, apiKey, "metric");
 
         call.enqueue(new Callback<Tiempo>() {
-
-
             @Override
             public void onResponse(Call<Tiempo> call, Response<Tiempo> response) {
                 if (response.isSuccessful() && response.body() != null) {
                     Tiempo tiempo = response.body();
-                    mostrarDatosTextfield.setText(String.format("Temperatura en El Ejido es: %.2f°C",
-                            tiempo.main.temp));
+                    mostrarDatosTextfield.setText(String.format("Temperatura en %s es: %.2f°C", ciudad, tiempo.main.temp));
+                } else {
+                    mostrarDatosTextfield.setText("Error al obtener el clima de dicha. Ciudad no encontrada. Vuelva a intentarlo por favor");
                 }
             }
 
             @Override
             public void onFailure(Call<Tiempo> call, Throwable t) {
-                System.out.println("Error de conexión: " + t.getMessage());
+                mostrarDatosTextfield.setText("Error de conexión: " + t.getMessage());
             }
         });
     }
